01699a82ad74bf675aff99c65c58aa0e9aaf0fad
LukasSemler <semler.l04@htlwienwest.at>
2022-09-23 18:39:52 +0200

    DatePicker installiert

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -4,6 +4,8 @@ import router from './router';
 import './assets/css/index.css';
 import { createPinia } from 'pinia';
 import axios from 'axios';
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 
 //Base-URL axios
 if (location.origin === 'http://localhost:8080' || location.origin === 'http://localhost:5050') {
@@ -17,5 +19,7 @@ const pinia = createPinia();
 app.use(pinia);
 
 app.use(router);
+
+app.component('Datepicker', Datepicker);
 
 app.mount('#app');
